121122d5b462e673db7bdcd7ce673107e206373f
RuslanaMav <116889933+RuslanaMav@users.noreply.github.com>
2023-02-14 12:06:32 +0500

    feat: add code

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+ let s2='';
+    function decoder(str){
+        let s1='';
+        let s2=''
+        while (str.length!=0){
+            if (str.slice(0,2).localeCompare('10')==0) s1+='.';
+            if (str.slice(0,2).localeCompare('11')==0) s1+='-';
+            str=str.substr(2,str.length);}
+        for (key in MORSE_TABLE )
+            if (s1.localeCompare(key)==0) s2+=MORSE_TABLE [key];
+           return s2;} 
+           
+    while (expr.length!=0) {
+        if (expr.slice(0,10).localeCompare('**********')==0) { 
+            s2+=' ';  
+            expr=expr.substr(10,expr.length);}
+        else {
+        s2+=decoder(expr.slice(0,10));
+        expr=expr.substr(10,expr.length);}}
+        return s2;  
 }
 
 module.exports = {
